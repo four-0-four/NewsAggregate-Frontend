6b56f0bc6cacd56c33a69ed577dce28dc1da69ef
Sina Rafiei <msina.raf@gmail.com>
2024-03-15 09:50:05 -0400

    fix the issue with unauthenticated news not being centered

--- src/pages/news/news.tsx
+++ src/pages/news/news.tsx
@@ -80,7 +80,7 @@ const News: React.FC<NewsComponentProps> = ({}) => {
     }
     
     return (
-        <div className={`flex flex-col mx-auto lg:ml-0 lg:mr-auto md:max-w-2xl lg:max-w-2xl xl:max-w-3xl w-full rounded-[20px] bg-white border-solid border border-gray-100 overflow-hidden p-2 pb-6 ${isAuthenticated?"":"mx-auto"}`}>
+        <div className={`flex flex-col md:max-w-2xl lg:max-w-2xl xl:max-w-3xl w-full rounded-[20px] bg-white border-solid border border-gray-100 overflow-hidden p-2 pb-6 ${isAuthenticated?"lg:ml-0 lg:mr-auto":"mx-auto"} `}>
             <div className="relative rounded-[20px] max-h-[300px] overflow-hidden">
                 <img src={imageSrc} alt="News" className="block object-cover object-center rounded-[20px] max-h-[300px] w-full" />
                 <h2 className="hidden xs:block absolute bottom-0 left-0 right-0 block md:block text-lg sm:text-xl font-bold text-white p-4 pt-32"
